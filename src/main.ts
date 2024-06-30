cded169d03e23294b5ba2a98cd070301aa82a585
EvgeniyLyapunov <lyapunovcs@gmail.com>
2024-06-30 11:04:06 +0300

    add libreries and pockets, add auto-import routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
+
+const vuetify = createVuetify({
+	components,
+	directives,
+	icons: {
+		defaultSet: 'mdi',
+	},
+});
+
 import App from './App.vue';
 import router from './router';
 
@@ -8,5 +23,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueRouter from 'unplugin-vue-router/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	plugins: [
+		VueRouter({
+			importMode: 'sync',
+			dts: './src/typed-router.d.ts',
+			routesFolder: 'src/pages/views',
+		}),
+		vue(),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+});
