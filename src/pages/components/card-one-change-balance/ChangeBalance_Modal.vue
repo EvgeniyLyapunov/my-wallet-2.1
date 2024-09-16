d3f45759aed141fc7839fdae9f069c7b63a9a91a
EvgeniyLyapunov <lyapunovcs@gmail.com>
2024-09-16 22:00:20 +0300

    work over change balance modal

--- src/pages/components/card-one-change-balance/ChangeBalance_Modal.vue
+++ src/pages/components/card-one-change-balance/ChangeBalance_Modal.vue
@@ -15,44 +15,59 @@
 							}).format(props.card!.currentSum)
 						}}</span>
 					</div>
-					<input
-						v-model="inputAmount"
-						class="modal__form-input"
-						type="text"
-						placeholder="operation amount"
-					/>
-					<div class="modal__form-actions">
-						<v-btn class="modal__form-actions-btn" icon="mdi-minus-thick" rounded="0"></v-btn>
+					<div
+						:class="{
+							'modal__form-input': !isFocus_InputAmountField,
+							'modal__form-input_focus': isFocus_InputAmountField,
+						}"
+					>
+						<span>{{ inputAmount }}</span>
+					</div>
+					<div class="modal__form-actions mb-5">
+						<v-btn class="modal__form-actions-btn">
+							<v-icon>mdi-minus-thick</v-icon>
+						</v-btn>
 						<v-btn class="modal__form-actions-btn modal__form-actions-btn_balance"
 							>Change Balance</v-btn
 						>
-						<v-btn class="modal__form-actions-btn" icon="mdi-plus-thick" rounded="0"></v-btn>
+						<v-btn class="modal__form-actions-btn">
+							<v-icon>mdi-plus-thick</v-icon>
+						</v-btn>
 					</div>
 					<div class="modal__form-keyboard">
 						<div class="modal__form-keyboard-line mb-3">
-							<v-btn>1</v-btn>
-							<v-btn class="mr-3 ml-3">2</v-btn>
-							<v-btn>3</v-btn>
+							<v-btn class="modal__form-keyboard-btn" @click="onNumberKeyPress('1')">1</v-btn>
+							<v-btn class="modal__form-keyboard-btn mr-3 ml-3" @click="onNumberKeyPress('2')"
+								>2</v-btn
+							>
+							<v-btn class="modal__form-keyboard-btn" @click="onNumberKeyPress('3')"> 3</v-btn>
 						</div>
 						<div class="modal__form-keyboard-line mb-3">
-							<v-btn>4</v-btn>
-							<v-btn class="mr-3 ml-3">5</v-btn>
-							<v-btn>6</v-btn>
+							<v-btn class="modal__form-keyboard-btn" @click="onNumberKeyPress('4')">4</v-btn>
+							<v-btn class="modal__form-keyboard-btn mr-3 ml-3" @click="onNumberKeyPress('5')"
+								>5</v-btn
+							>
+							<v-btn class="modal__form-keyboard-btn" @click="onNumberKeyPress('6')">6</v-btn>
 						</div>
 						<div class="modal__form-keyboard-line mb-3">
-							<v-btn>7</v-btn>
-							<v-btn class="mr-3 ml-3">8</v-btn>
-							<v-btn>9</v-btn>
+							<v-btn class="modal__form-keyboard-btn" @click="onNumberKeyPress('7')">7</v-btn>
+							<v-btn class="modal__form-keyboard-btn mr-3 ml-3" @click="onNumberKeyPress('8')"
+								>8</v-btn
+							>
+							<v-btn class="modal__form-keyboard-btn" @click="onNumberKeyPress('9')">9</v-btn>
 						</div>
-						<div class="modal__form-keyboard-line modal__form-keyboard-line_last">
-							<v-btn class="modal__form-keyboard-line_last-btn mr-3">0</v-btn>
-							<v-btn prepend-icon="mdi-arrow-left-bold-outline" width="100"></v-btn>
+						<div class="modal__form-keyboard-line mb-3">
+							<v-btn class="modal__form-keyboard-btn" @click="onNumberKeyPress('0')">0</v-btn>
+							<v-btn class="modal__form-keyboard-btn mr-3 ml-3" @click="onLastNumberDelete">
+								<v-icon>mdi-arrow-left-bold-outline</v-icon>
+							</v-btn>
+							<v-btn class="modal__form-keyboard-btn" @click="onResetInputAmount">C</v-btn>
 						</div>
 					</div>
 				</v-card-text>
 				<div class="modal__footer">
 					<div class="modal__footer-btns">
-						<v-btn class="modal__footer-btn">Cancel</v-btn>
+						<v-btn class="modal__footer-btn" @click="onCloseModal">Cancel</v-btn>
 					</div>
 				</div>
 			</v-card>
@@ -82,7 +97,45 @@
 		},
 	});
 
-	const inputAmount = ref<number | undefined>();
+	const inputAmount = ref<string>('0');
+	const isFocus_InputAmountField = computed<boolean>(() => {
+		return inputAmount.value === '0' ? false : true;
+	});
+
+	const onNumberKeyPress = (keyNumber: string) => {
+		if (inputAmount.value === '0' && keyNumber === '0') {
+			return;
+		}
+
+		if (inputAmount.value === '0') {
+			inputAmount.value = keyNumber;
+			return;
+		}
+
+		inputAmount.value = inputAmount.value + keyNumber;
+	};
+
+	const onLastNumberDelete = () => {
+		if (inputAmount.value === '0') {
+			return;
+		}
+
+		if (inputAmount.value.length === 1) {
+			inputAmount.value = '0';
+			return;
+		}
+
+		inputAmount.value = inputAmount.value.slice(0, inputAmount.value.length - 1);
+	};
+
+	const onResetInputAmount = () => {
+		inputAmount.value = '0';
+	};
+
+	const onCloseModal = () => {
+		inputAmount.value = '0';
+		emit('update:modelValue', false);
+	};
 </script>
 
 <style scoped lang="scss">
